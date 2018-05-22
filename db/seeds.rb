57c9f6f7d51037e8d07f54ed88fe649e0ff84cdc
Minh <minhdc@atmarkcafe.org>
2018-05-22 11:19:03 +0700

    Exercise Category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 Product.destroy_all
-table = 'products'
+Category.destroy_all
+table1 = 'products'
+table2 = 'categories'
 auto_inc_val = 1
 ActiveRecord::Base.connection.execute(
-  "ALTER SEQUENCE #{table}_id_seq RESTART WITH #{auto_inc_val}"
+  "ALTER SEQUENCE #{table1}_id_seq RESTART WITH #{auto_inc_val}"
 )
-Product.create(title: 'Runy Book', description: '<h>Learn Ruby programing</h>', price: 12.99, published: true)
-Product.create(title: 'JQuery Book', description: 'Learn <i>JQuery', price: 11.99, published: true)
-Product.create(title: 'SASS Book', description: 'Learn CSS<br/> with SASS', price: 19.99, published: true)
+ActiveRecord::Base.connection.execute(
+  "ALTER SEQUENCE #{table2}_id_seq RESTART WITH #{auto_inc_val}"
+)
+
+c = Category.create(name: 'Programing languages')
+c.products.create(title: 'Papers', description: 'description about papers', price: 10, published: true)
+
+p = Array.new
+p << Product.create(title: 'Runy Book', description: '<h>Learn Ruby programing</h>', price: 12.99, published: true)
+p << Product.create(title: 'JQuery Book', description: 'Learn <i>JQuery', price: 11.99, published: true)
+p << Product.create(title: 'SASS Book', description: 'Learn CSS<br/> with SASS', price: 19.99, published: true)
+
+p.each { |i| c.products << p }
+puts 'Category title: ' + Product.first.category.inspect
+puts Category.first.products.inspect
