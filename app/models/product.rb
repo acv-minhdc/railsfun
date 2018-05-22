bf175f0b71267ac17cbfcabd1b2471619fc8186a
Minh <minhdc@atmarkcafe.org>
2018-05-22 10:11:45 +0700

    Add seed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   validates :title, :description, presence: true
   validate :title_is_shorter_than_description
+  validates :price, presence: true, numericality: { greater_than: 0 }
 
   def title_is_shorter_than_description
     return if title.blank? || description.blank?
@@ -10,4 +11,5 @@ class Product < ApplicationRecord
     return if description.length >= title.length
     errors.add(:description, 'can\'t be shorter than title')
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+table = 'products'
+auto_inc_val = 1
+ActiveRecord::Base.connecton.execute(
+  "ALTER SEQUENCE #{table}_id_seq RESTART WITH #{auto_inc_val}"
+)
+Product.create(title: 'Runy Book', description: 'Learn Ruby programing', price: 12.99, published: true)
+Product.create(title: 'JQuery Book', description: 'Learn JQuery', price: 11.99, published: true)
+Product.create(title: 'SASS Book', description: 'Learn CSS with SASS', price: 19.99, published: true)
