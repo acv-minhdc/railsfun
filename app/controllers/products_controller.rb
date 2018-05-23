2dbcfd8b527f3cca56d7d0083d24666fe206705a
Minh <minhdc@atmarkcafe.org>
2018-05-23 15:15:17 +0700

    show product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,8 @@ class ProductsController < ApplicationController
   def index
     @products = Product.includes(:category).where(published: true)
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
 end
